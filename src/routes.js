b05737e83c21c3d9b7555173493ce0e282d1f3fb
Alexander Lyons <alexander.n.lyons@gmail.com>
2017-03-05 09:00:24 -0500

    Update routes with components

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+import Layout from './components/Layout';
+import HomePage from './components/HomePage';
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
     <Route path="/" component={Layout}>
-        <IndexRoute component={IndexPage}/>
+        <IndexRoute component={HomePage}/>
         <Route path="*" component={NotFoundPage}/>
     </Route>
 );
